35600991c9ad053021328918290f433938788aa4
therealmensumit <sk03950@gmail.com>
2024-09-13 12:02:23 +0530

    "Added ScrollToTop component to Layout component"

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { ScrollToTop } from "../ScrollToTop";
 
 export const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <ScrollToTop />
     </>
   );
 };
